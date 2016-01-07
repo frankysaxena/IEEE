12804cdb6457d06f65c840a523d91123b33b97cc
frankysaxena <frankysaxena@gmail.com>
2016-01-07 21:39:50 +0000

    testing mail

--- ucl-ieee-web/index.php
+++ ucl-ieee-web/index.php
@@ -1,3 +1,24 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = "uclieeesb@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $findus = $_POST['find-us']
+    $news = $_POST['news']
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$findus,$news,$headers);
+    mail($from,$subject2,$message2,$findus,$news,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -361,24 +382,3 @@
         </section>
     </body>
 </html>
-
-<?php
-if(isset($_POST['submit'])){
-    $to = "uclieeesb@gmail.com";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $findus = $_POST['find-us']
-    $news = $_POST['news']
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$findus,$news,$headers);
-    mail($from,$subject2,$message2,$findus,$news,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
