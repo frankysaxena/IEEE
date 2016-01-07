be06d41f2a154ed8aa9b9fad8dd5788be2d76337
frankysaxena <frankysaxena@gmail.com>
2016-01-07 23:10:31 +0000

    fixed a different }

--- ucl-ieee-web/contact_form_handler.php
+++ ucl-ieee-web/contact_form_handler.php
@@ -16,13 +16,22 @@ if ($newsletter != 'Yes') {
     $newsletter = 'No';
 }
 
+
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
 if( empty($errors))
 {
     $to = $myemail;
     $email_subject = "Contact form submission: $name";
     $email_body = "You have received a new message. ".
     " Here are the details:\n Name: $name \n Email: $email_address \n Message: $message \n Find us: $findus \n Newsletter: $newsletter"
-
+}
     ;
 
 
@@ -31,5 +40,5 @@ if( empty($errors))
 
     mail($to,$email_subject,$email_body,$headers);
     //redirect to the 'thank you' page
-}
+
 ?>
